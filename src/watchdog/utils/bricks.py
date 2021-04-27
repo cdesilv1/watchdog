b72ca49ddc8b01f8bd18b9bbe85e4d5d837a47b1
cdesilv1 <chris.desilva_@outlook.com>
2021-04-27 13:40:55 -0700

    changed SkipRepeatsQueue impl.

--- src/watchdog/utils/bricks.py
+++ src/watchdog/utils/bricks.py
@@ -38,6 +38,66 @@ Classes
 
 import queue
 
+import collections
+
+class OrderedSet(collections.MutableSet):
+
+    def __init__(self, iterable=None):
+        self.end = end = [] 
+        end += [None, end, end]         # sentinel node for doubly linked list
+        self.map = {}                   # key --> [key, prev, next]
+        if iterable is not None:
+            self |= iterable
+
+    def __len__(self):
+        return len(self.map)
+
+    def __contains__(self, key):
+        return key in self.map
+
+    def add(self, key):
+        if key not in self.map:
+            end = self.end
+            curr = end[1]
+            curr[2] = end[1] = self.map[key] = [key, curr, end]
+
+    def discard(self, key):
+        if key in self.map:        
+            key, prev, next = self.map.pop(key)
+            prev[2] = next
+            next[1] = prev
+
+    def __iter__(self):
+        end = self.end
+        curr = end[2]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[2]
+
+    def __reversed__(self):
+        end = self.end
+        curr = end[1]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[1]
+
+    def pop(self, last=True):
+        if not self:
+            raise KeyError('set is empty')
+        key = self.end[1][0] if last else self.end[2][0]
+        self.discard(key)
+        return key
+
+    def __repr__(self):
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, list(self))
+
+    def __eq__(self, other):
+        if isinstance(other, OrderedSet):
+            return len(self) == len(other) and list(self) == list(other)
+        return set(self) == set(other)
+
 
 class SkipRepeatsQueue(queue.Queue):
 
@@ -84,19 +144,8 @@ class SkipRepeatsQueue(queue.Queue):
 
     def _init(self, maxsize):
         super()._init(maxsize)
-        self._last_item = None
-
+        self.queue = OrderedSet()
     def _put(self, item):
-        if item != self._last_item:
-            super()._put(item)
-            self._last_item = item
-        else:
-            # `put` increments `unfinished_tasks` even if we did not put
-            # anything into the queue here
-            self.unfinished_tasks -= 1
-
+        self.queue.add(item)
     def _get(self):
-        item = super()._get()
-        if item is self._last_item:
-            self._last_item = None
-        return item
+        return self.queue.pop()
